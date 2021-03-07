2ed2a09addbf949385387c2ed3fad675a858486c
Tongkai-Z <ztksticktoit@gmail.com>
2021-03-07 17:05:43 +0800

    coordinator 0.1

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,21 +1,120 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"errors"
+	"log"
+	"math"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"strconv"
+	"sync"
+)
 
+// IDLE map task status
+const IDLE = 1
+
+// INPROGRESS map task status
+const INPROGRESS = 2
+
+// COMPLETED map task status
+const COMPLETED = 3
 
 type Coordinator struct {
 	// Your definitions here.
-
+	reduceTaskQueue  chan Task
+	mapTaskQueue     chan Task
+	mapTaskStatus    map[string]*Task
+	reduceTaskStatus map[string]*Task
+	mapLeft          int
+	reduceLeft       int
+	done             bool
+	mu               sync.Mutex
 }
 
-// Your code here -- RPC handlers for the worker to call.
+type Task struct {
+	Files  []string
+	IsMap  bool
+	ID     string
+	Status int
+}
 
-//
-// an example RPC handler.
+// RequestTask work wait to be assigned a task
+func (c *Coordinator) RequestTask(req *RequestTaskRequest, res *RequestTaskResponse) error {
+	// if there are mapping task left assign one map task to the work, otherwise assign reduce task
+	c.mu.Lock()
+	ml := c.mapLeft
+	rl := c.reduceLeft
+	cd := c.done
+	c.mu.Unlock()
+	if cd {
+		return errors.New("Map reduce is finished")
+	}
+	if ml > 0 {
+		task, ok := <-c.mapTaskQueue
+		if !ok {
+			res.Task = nil
+			return nil
+		}
+		res.Task = &task
+	} else if rl > 0 {
+		//assign reduce
+		task, ok := <-c.reduceTaskQueue
+		if !ok {
+			return errors.New("all job has been finished")
+		}
+		res.Task = &task
+	} else {
+		return errors.New("all job has been finished")
+	}
+
+	return nil
+}
+
+// FinishMap map task output mr-X-Y, x is task id, Y is the hash value
+func (c *Coordinator) FinishMap(req *FinishMapRequest, res *FinishMapResponse) error {
+	task := req.Task
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if _, ok := c.mapTaskStatus[task.ID]; ok {
+		c.mapTaskStatus[task.ID].Status = COMPLETED
+		c.mapLeft--
+		if c.mapLeft == 0 {
+			close(c.mapTaskQueue)
+		}
+		// add the intermediateFiles to corresponding reduce task
+		for filename, hashvalue := range req.IntermediateFiles {
+			if reduceTask, ok := c.reduceTaskStatus[hashvalue]; ok {
+				reduceTask.Files = append(reduceTask.Files, filename)
+			} else {
+				reduceTask := Task{}
+				reduceTask.IsMap = false
+				reduceTask.Files = []string{}
+				reduceTask.Files = append(reduceTask.Files, filename)
+				reduceTask.Status = IDLE
+				c.reduceTaskStatus[hashvalue] = &reduceTask
+				c.reduceTaskQueue <- reduceTask
+				c.reduceLeft++
+			}
+		}
+	}
+	// get the map result file and put them in corresponding reduce task
+	return nil
+}
+
+// FinishReduce notify master that reduce is finished
+func (c *Coordinator) FinishReduce(req *FinishReduceRequest, res *FinishReduceResponse) error {
+	c.mu.Lock()
+	c.reduceLeft--
+	if c.reduceLeft == 0 {
+		close(c.reduceTaskQueue)
+		c.done = true
+	}
+	c.mu.Unlock()
+	return nil
+}
+
 //
 // the RPC argument and reply types are defined in rpc.go.
 //
@@ -24,13 +123,12 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	return nil
 }
 
-
 //
 // start a thread that listens for RPCs from worker.go
 //
 func (c *Coordinator) server() {
-	rpc.Register(c)
-	rpc.HandleHTTP()
+	rpc.Register(c)  // publish the receiver's method in ther server
+	rpc.HandleHTTP() //register http handler in the server
 	//l, e := net.Listen("tcp", ":1234")
 	sockname := coordinatorSock()
 	os.Remove(sockname)
@@ -47,10 +145,11 @@ func (c *Coordinator) server() {
 //
 func (c *Coordinator) Done() bool {
 	ret := false
-
-	// Your code here.
-
-
+	c.mu.Lock()
+	if c.done {
+		ret = true
+	}
+	c.mu.Unlock()
 	return ret
 }
 
@@ -61,10 +160,40 @@ func (c *Coordinator) Done() bool {
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-
-	// Your code here.
-
-
+	c.initializeMapTask(files, nReduce)
+	c.reduceTaskQueue = make(chan Task, nReduce)
+	c.reduceTaskStatus = make(map[string]*Task)
+	c.done = false
 	c.server()
 	return &c
+}
+
+//
+// we have nReduce map tasks, thus each task should have ceil(len(files)/nReduce) files
+//
+func (c *Coordinator) initializeMapTask(files []string, nReduce int) {
+	c.mapTaskQueue = make(chan Task, nReduce)
+	c.mapTaskStatus = make(map[string]*Task)
+	id := 0
+	interval := int(math.Ceil(float64(len(files)) / float64(nReduce)))
+	i := 0
+	for i < len(files) {
+		task := Task{}
+		task.Files = files[i:min(i+interval, len(files))]
+		task.IsMap = true
+		task.ID = strconv.Itoa(id)
+		task.Status = IDLE
+		c.mapTaskQueue <- task
+		c.mapTaskStatus[task.ID] = &task
+		i += interval
+		id++
+	}
+	c.mapLeft = len(c.mapTaskQueue)
+}
+
+func min(x int, y int) int {
+	if x > y {
+		return y
+	}
+	return x
 }
--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -22,8 +24,25 @@ type ExampleReply struct {
 	Y int
 }
 
-// Add your RPC definitions here.
+type RequestTaskRequest struct{}
 
+type RequestTaskResponse struct {
+	Task *Task
+}
+
+type FinishMapRequest struct {
+	Task              *Task
+	IntermediateFiles map[string]string //name --> hashvalue
+}
+type FinishMapResponse struct{}
+
+type FinishReduceRequest struct {
+	Task  *Task
+	Files []string
+}
+type FinishReduceResponse struct{}
+
+// Add your RPC definitions here.
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
