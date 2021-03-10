0462da5b554a3febee994948e6deeb5f025b4dc3
Tongkai-Z <ztksticktoit@gmail.com>
2021-03-10 15:24:03 +0800

    add crash recovery and time wait for work loop

--- src/main/scratch.go
+++ src/main/scratch.go
@@ -1,32 +1,25 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
-
-	"6.824/mr"
+	"hash/fnv"
+	"time"
 )
 
 func main() {
-	file3, _ := os.Create("mr-0-0")
-	enc := json.NewEncoder(file3)
+	fmt.Printf("%v", wait())
+}
+func wait() int {
+	defer func() {
+		fmt.Printf("I am waiting for 4 secs\n")
+		time.Sleep(4 * time.Second)
+		fmt.Printf("time is up\n")
+	}()
+	return 1
+}
 
-	kv := mr.KeyValue{"mesdf", "1"}
-	enc.Encode(&kv)
-	enc2 := json.NewEncoder(file3)
-	kv = mr.KeyValue{"123123", "1"}
-	enc2.Encode(&kv)
-	file3.Close()
-	kva := []mr.KeyValue{}
-	f, _ := os.Open("mr-0-0")
-	dec := json.NewDecoder(f)
-	for {
-		var kv mr.KeyValue
-		if err := dec.Decode(&kv); err != nil {
-			break
-		}
-		kva = append(kva, kv)
-	}
-	fmt.Printf("%v", kva)
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
 }
